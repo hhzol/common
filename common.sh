575c5363973d4169329c31e8a76c8bccdf40fe8d
hhzol <100747442+hhzol@users.noreply.github.com>
2024-06-25 16:16:54 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -349,7 +349,7 @@ echo "二、删除theme1中多余的主题和插件..."
 echo "theme1保留的主题有：luci-theme-argon,luci-theme-Butterfly,luci-theme-Butterfly-dark,luci-theme-Light,luci-theme-argon-dark-mod,luci-theme-argon-light-mod,luci-theme-bootstrap-mod"
 
 #删除asterisk
-rm -rf ${HOME_PATH}/package/feeds/telephony/asterisk
+rm -rf ${HOME_PATH}/package/feeds/telephony/{asterisk}
 echo "asterisk已删除"
 
 #onecloud
