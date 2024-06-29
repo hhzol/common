929d9fd55f5dcde528b455de914b07d9989e18d0
hhzol <100747442+hhzol@users.noreply.github.com>
2024-06-29 08:32:18 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -530,7 +530,7 @@ cp -Rf ${HOME_PATH}/feeds.conf.default ${HOME_PATH}/LICENSES/doc/uniq.conf
 function Diy_Wenjian() {
 cp -Rf ${HOME_PATH}/LICENSES/doc/uniq.conf ${HOME_PATH}/feeds.conf.default
 
-sed -i "s/+libpcre //g" ${HOME_PATH}/package/feeds/telephony/freeswitch/Makefile
+sed -i "s/+libpcre //g" ${HOME_PATH}/feeds/telephony/freeswitch/Makefile
 
 # 增加中文语言包
 if [[ -f "${HOME_PATH}/feeds/luci/modules/luci-mod-system/root/usr/share/luci/menu.d/luci-mod-system.json" ]]; then
