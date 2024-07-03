7253465e28cecf611b3319e2753909a22d98eb10
hhzol <100747442+hhzol@users.noreply.github.com>
2024-07-03 08:07:09 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -657,18 +657,18 @@ esac
 
 source ${HOME_PATH}/build/common/Share/19.07/netsupport.sh
 
-[[ -d "${HOME_PATH}/build/common/Share/luci-app-samba4" ]] && rm -rf ${HOME_PATH}/build/common/Share/luci-app-samba4
-amba4="$(find . -type d -name 'luci-app-samba4')"
-autosam="$(find . -type d -name 'autosamba')"
-if [[ -z "${amba4}" ]] && [[ -n "${autosam}" ]]; then
-  for X in "$(find . -type d -name 'autosamba')/Makefile"; do sed -i "s?+luci-app-samba4?+luci-app-samba?g" "$X"; done
-else
-  for X in "$(find . -type d -name 'autosamba')/Makefile"; do
-    if [[ `grep -c "+luci-app-samba4" $X` -eq '0' ]]; then
-      sed -i "s?+luci-app-samba?+luci-app-samba4?g" "$X"
-    fi
-  done
-fi
+#[[ -d "${HOME_PATH}/build/common/Share/luci-app-samba4" ]] && rm -rf ${HOME_PATH}/build/common/Share/luci-app-samba4
+#amba4="$(find . -type d -name 'luci-app-samba4')"
+#autosam="$(find . -type d -name 'autosamba')"
+#if [[ -z "${amba4}" ]] && [[ -n "${autosam}" ]]; then
+#  for X in "$(find . -type d -name 'autosamba')/Makefile"; do sed -i "s?+luci-app-samba4?+luci-app-samba?g" "$X"; done
+#else
+#  for X in "$(find . -type d -name 'autosamba')/Makefile"; do
+#    if [[ `grep -c "+luci-app-samba4" $X` -eq '0' ]]; then
+#      sed -i "s?+luci-app-samba?+luci-app-samba4?g" "$X"
+#    fi
+#  done
+#fi
 
 # files大法，设置固件无烦恼
 if [ -n "$(ls -A "${BUILD_PATH}/patches" 2>/dev/null)" ]; then
