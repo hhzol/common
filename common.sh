9c966ff8ee8f82ef8a39e6662a7bd2a11279d9b7
hhzol <100747442+hhzol@users.noreply.github.com>
2025-12-03 07:08:47 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -181,8 +181,8 @@ else
   gitsvn https://github.com/281677160/luci-theme-argon/tree/18.06 "${HOME_PATH}/package/luci-theme-argon"
 fi
 
+git clone https://git.zx2c4.com/wireguard-go
 echo "src-git alist https://github.com/sbwml/luci-app-openlist2.git;main" >> "${HOME_PATH}/feeds.conf.default"
-echo "src-git wireguard https://git.zx2c4.com/wireguard-go.git;master" >> "${HOME_PATH}/feeds.conf.default"
 echo "src-git danshui https://github.com/281677160/openwrt-package.git;$SOURCE" >> "${HOME_PATH}/feeds.conf.default"
 echo "src-git dstheme https://github.com/281677160/openwrt-package.git;$THEME_BRANCH" >> "${HOME_PATH}/feeds.conf.default"
 [[ "${OpenClash_branch}" == "1" ]] && echo "src-git OpenClash https://github.com/vernesong/OpenClash.git;master" >> "${HOME_PATH}/feeds.conf.default"
