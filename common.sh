dc01a9d751e67ba246b54db810eef96a9cb3fd5a
hhzol <100747442+hhzol@users.noreply.github.com>
2026-01-22 16:31:17 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -1082,7 +1082,7 @@ fi
 if [[ `grep -c "CONFIG_PACKAGE_luci-theme-argon=y" ${HOME_PATH}/.config` -eq '1' ]]; then
   pmg="$(date +%M | grep -o '.$').jpg"
   [[ ! -d "${HOME_PATH}/files/www/luci-static/argon/background" ]] && mkdir -p "${HOME_PATH}/files/www/luci-static/argon/background"
-  cp -Rf "$LINSHI_COMMON/Share/argon/jpg/${pmg}" "${HOME_PATH}/files/www/luci-static/argon/background/argon.jpg"
+  cp -Rf "$LINSHI_COMMON/Share/argon/jpg/argon.jpg" "${HOME_PATH}/files/www/luci-static/argon/background/argon.jpg"
   if [[ $? -ne 0 ]]; then
     echo "拉取文件错误,请检测网络"
     exit 1
