7c0efdc4d58c2065548ed67ed2afbab9d27db83a
hhzol <100747442+hhzol@users.noreply.github.com>
2026-01-24 19:25:20 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -204,6 +204,7 @@ fi
 rm -rf feeds/packages/net/vlmcsd
 rm -rf feeds/luci/applications/luci-app-vlmcsd
 git clone --depth=1 https://github.com/immortalwrt/packages.git /tmp/imm-packages
+mkdir -p package/feeds/vlmcsd
 cp -a /tmp/imm-packages/net/vlmcsd package/feeds/vlmcsd
 rm -rf /tmp/imm-packages
 git clone --depth=1 https://github.com/immortalwrt/luci.git /tmp/imm-luci
