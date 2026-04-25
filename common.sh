36fba53a98bfa99f37151178f38fd07c71cd6183
hhzol <100747442+hhzol@users.noreply.github.com>
2026-04-25 21:58:25 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -197,6 +197,7 @@ echo "src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2.gi
 echo "src-git kms https://github.com/gaoderby/luci-app-kms;main" >> "${HOME_PATH}/feeds.conf.default"
 echo "src-git netwizard https://github.com/sirpdboy/luci-app-netwizard;main" >> "${HOME_PATH}/feeds.conf.default"
 git clone --depth=1 https://github.com/hhzol/luci-app-usb_printer.git ${HOME_PATH}/package/usb_printer
+git clone --depth=1 https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git ${HOME_PATH}/package/luci-app-unblockneteasemusic
 if grep -q "armvirt=y" $MYCONFIG_FILE || grep -q "armsr=y" $MYCONFIG_FILE; then
   git clone --depth=1 https://github.com/ophub/luci-app-amlogic.git ${HOME_PATH}/package/luci-app-amlogic
 fi
